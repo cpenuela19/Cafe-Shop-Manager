a66d89839daff846f71a6706d3422d2c35b55ac2
cpenuela19 <ce.penuela20@uniandes.edu.co>
2026-03-25 09:26:09 -0500

    test

--- src/main/java/com/charlytech/cafemanager/entities/UserEntity.java
+++ src/main/java/com/charlytech/cafemanager/entities/UserEntity.java
@@ -1,12 +1,10 @@
 package com.charlytech.cafemanager.entities;
-
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
-
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Data
@@ -19,16 +17,17 @@ public class UserEntity extends BaseEntity {
         BAKER,
         VENDOR
     }
+
     // Attributes
     private String username;
     private String email;
     private String password;
     private Role role;
     private boolean active;
-    private Date createdAt;
+    private LocalDateTime createdAt;
 
-    // Relationships
-    @OneToMany(mappedBy = "user")
+    // Relationships --- Here I used fetch = FetchType.LAZY just to point it up.
+    // By default, in this relationship (OneToMany from this side) is going to be loaded as LAZY.
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<IngredientPriceHistoryEntity> ingredientPrices = new ArrayList<>();
-    //test
 }
