897bdaf4ec1059654e8dafd42a540dfd7b815860
cpenuela19 <ce.penuela20@uniandes.edu.co>
2026-03-26 21:29:55 -0500

    Beginning Repositories for the *persistence-layer*

---
+++ src/main/java/com/charlytech/cafemanager/repositories/SupplierIngredientRepository.java
@@ -0,0 +1,9 @@
+package com.charlytech.cafemanager.repositories;
+
+import com.charlytech.cafemanager.entities.SupplierIngredientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierIngredientRepository extends JpaRepository<SupplierIngredientEntity, Long> {
+}
